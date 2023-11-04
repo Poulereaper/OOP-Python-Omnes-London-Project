fdbcde02ec75dbed77b881daad107e7c2bea4193
Poulereaper <mathis.grs05@gmail.com>
2023-11-04 03:19:48 +0100

    display mehoed

--- Project_OOP_fp.py
+++ Project_OOP_fp.py
@@ -3,6 +3,7 @@ import pymysql
 import tkinter as tk
 import tkinter.messagebox
 
+
 class MyGUI:
     def __init__(self):
         # Create the main window widget.
@@ -11,20 +12,17 @@ class MyGUI:
         self.main_window.geometry("900x500")
         # Create a frame at the top for buttons
         self.top_frame = tk.Frame(self.main_window)
-        self.top_frame.pack(side=tk.TOP, fill=tk.X)
+        
         # Create buttons
         self.my_button_Log = tk.Button(self.top_frame, text='Sign In or Sign Up', command=self.do_something_one)
         self.my_button_Search = tk.Button(self.top_frame, text='Advanced Search', command=self.do_something_two)
 
-        # Pack the 'Sign In or Sign Up' button to the left (west)
-        self.my_button_Log.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=10, pady=10)
-        # Pack the 'Advanced Search' button to the right (east)
-        self.my_button_Search.pack(ipadx=5, ipady=5, side=tk.RIGHT, padx=10, pady=10)
+        
 
         #Creat a list box of the days of the week
         self.label = tk.Label(self.main_window, text="Select your favorite day")
         self.listbox = tk.Listbox(self.main_window, height=0, width=0)
-        self.listbox.pack()
+        
         day=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
         for item in day:
@@ -33,7 +31,17 @@ class MyGUI:
          
         #Now I want to create a button that will print the selected item in the listbox
          #self.my_button = tkinter.Button(self.main_window, text='Click Me!', command=self.do_something)
+        
+
+    def display(self):
+        self.top_frame.pack(side=tk.TOP, fill=tk.X)
+        # Pack the 'Sign In or Sign Up' button to the left (west)
+        self.my_button_Log.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=10, pady=10)
+        # Pack the 'Advanced Search' button to the right (east)
+        self.my_button_Search.pack(ipadx=5, ipady=5, side=tk.RIGHT, padx=10, pady=10)
+        self.listbox.pack()
         tk.mainloop()
+
 
     def do_something_one(self):
         tk.messagebox.showinfo('Response', 'Thanks for clicking the button.')
@@ -45,4 +53,4 @@ class MyGUI:
 # Create an instance of the MyGUI class.
 if __name__ == '__main__':
     my_gui = MyGUI()
-    
+    my_gui.display()
